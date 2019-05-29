a35516e0403ac2454fa0848fc96a1c7bcbb92298
Tony Kieling <tony.kieling@gmail.com>
2019-05-28 18:33:05 -0700

    changes

--- Ch01/01_03/start/src/index.js
+++ Ch01/01_03/start/src/index.js
@@ -1,2 +1,10 @@
 import C from './constants.js'
 import { allSkiDays, goal} from './initialState.json'
+
+console.log(`
+  goal= ${goal}
+  many days: ${allSkiDays.length}
+
+  Constants (actions):
+  ${Object.keys(C).join('\n     ')}
+`)
